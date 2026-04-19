28b34bc7cca9bc247134098128cbfd915526899b
Arvind-Vishwa <av0216380@gmail.com>
2026-04-19 19:10:13 +0530

    created a login controller

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,13 +1,13 @@
 const userModel=require("../models/user.model")
 const bcrypt=require("bcrypt")
-
+const jwt=require('jsonwebtoken');
 
 
 async function registerController(req,res){
     const {username,email,password}=req.body;
 
     // search in databse
-    const isUserAlreadyExist=userModel.find({
+    const isUserAlreadyExist=await userModel.findOne({
         $or:[
             {username},
             {email}
@@ -23,9 +23,59 @@ async function registerController(req,res){
     // password hashed
     const hashedPass=await bcrypt.hash(password,10);
 
-    
+    const user=await userModel.create({
+        username,
+        email,
+        password:hashedPass
+    });
+
+    const token=jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET,{
+        expiresIn:24*24*60*60
+    })
+
+    res.cookie("token",token);
+
+    res.status(201).json({
+        message:"User registered succesfully"
+    });
+
 
 }
 
+async function loginController(req,res){
+    const {username,email,password}=req.body;
 
-module.exports={registerController}
+    const user=await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(!user){
+        return res.json({
+            message:"User not found in database"
+        })
+    }
+
+    const pass=await bcrypt.compare(password,user.password);
+    if(!pass){
+        return res.json({
+            message:"password is incorrect"
+        })
+    }
+    const token=jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET,
+    {expiresIn:24*24*60*60})
+
+    res.cookie("token",token);
+
+    res.status(200).json({
+        message:"user logged in sucessfully"
+    })
+}
+
+
+module.exports={registerController,loginController}
