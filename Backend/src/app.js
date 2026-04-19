c925010fbd3aa2c0e5b343e6cdc1cee61f689831
Arvind-Vishwa <av0216380@gmail.com>
2026-04-19 11:58:29 +0530

    added a cookie-parser to parse cookie data

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,13 @@
 const express=require('express')
 const app=express();
+const cookieParser=require('cookie-parser')
+const userRouter=require("./routes/user.route")
 
+app.use(cookieParser());
 app.use(express.json());
 
-
+// user registration
+app.use('/api',userRouter)
 
 
 module.exports=app;
