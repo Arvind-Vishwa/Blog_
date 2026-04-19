213e9a5531e75e7e07959ef28b983880a0b7b934
Arvind-Vishwa <av0216380@gmail.com>
2026-04-19 11:57:07 +0530

    created a routes for user schema

---
+++ Backend/src/routes/user.route.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const router=express.Router();
+const {registerController}=require("../controllers/user.controller")
+
+router.post('/register',registerController);
+
+
+
+
+
+
+
+module.exports=router;
