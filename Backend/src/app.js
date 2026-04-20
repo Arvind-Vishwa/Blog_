b262a63594b7dc45f99d7b35327c8716002a5b95
Arvind-Vishwa <av0216380@gmail.com>
2026-04-20 07:16:22 +0530

    added a api of post route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,18 @@
 const express=require('express')
 const app=express();
 const cookieParser=require('cookie-parser')
-const userRouter=require("./routes/user.route")
+
+
+const userRoute=require("./routes/user.route")
+const postRoute=require("./routes/post.route")
+
 
 app.use(cookieParser());
 app.use(express.json());
 
 // user registration
-app.use('/api',userRouter)
+app.use('/api',userRoute)
+app.use('/api/create',postRoute)
 
 
 module.exports=app;
